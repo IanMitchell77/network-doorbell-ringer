d9a84bf422bd3d7cefe6494da86397e1d496b275
Ian Mitchell <imi06@localhost.localdomain>
2017-02-02 17:04:12 +0000

    Fix timeout issue so it times out and closes properly and small code tidyup.

--- ringer.c
+++ ringer.c
@@ -20,14 +20,14 @@ int sockfd;
 
 void intHandler(int dummy) {
 	keepRunning = 0;
-	close(sockfd);
+	//close(sockfd);
 }
 
 #define MYPORT "4950"	// the port users will be connecting to
 
 #define MAXBUFLEN 30
 
-#define TIMEOUT_MS      1
+#define TIMEOUT_SECONDS      1
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -40,8 +40,7 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 int main(void)
-{
-	
+{	
 	struct addrinfo hints, *servinfo, *p;
 	int rv;
 	int numbytes;
@@ -52,7 +51,6 @@ int main(void)
 
 	signal(SIGINT, intHandler);
 
-   	
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
 	hints.ai_socktype = SOCK_DGRAM;
@@ -64,15 +62,20 @@ int main(void)
 	}
 
 	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
+	for(p = servinfo; p != NULL; p = p->ai_next) 
+	{
 		if ((sockfd = socket(p->ai_family, p->ai_socktype,
 				p->ai_protocol)) == -1) {
 			perror("listener: socket");
 			continue;
 		}
 
-		static int timeout = TIMEOUT_MS;
-		setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,(char*)&timeout,sizeof(timeout));
+		struct timeval timeout;      
+		timeout.tv_sec = TIMEOUT_SECONDS;
+		timeout.tv_usec = 0;
+
+		if (setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+			perror("setsockopt failed\n");
 
 		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
 			close(sockfd);
@@ -96,25 +99,14 @@ int main(void)
 		if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
 			(struct sockaddr *)&their_addr, &addr_len)) == -1) {
 			//perror("recvfrom");
-			exit(1);
+			//exit(1);
+		}
+		else if ( strncmp(buf, "doorbell-button-press", 21 ) == 0 )
+		{
+			printf("button push!\n");
+			system("aplay doorbell.wav");
 		}
 
-		/*printf("listener: got packet from %s\n",
-			inet_ntop(their_addr.ss_family,
-				get_in_addr((struct sockaddr *)&their_addr),
-				s, sizeof s));
-		printf("listener: packet is %d bytes long\n", numbytes);
-		buf[numbytes] = '\0';
-		printf("listener: packet contains \"%s\"\n", buf);*/
-
-		int rc = strncmp(buf, "doorbell-button-press", 21 );
-			if(rc == 0)
-			{
-				printf("button push!\n");
-				system("aplay doorbell.wav");
-			}
-
-		
 	}
 
 	close(sockfd);
